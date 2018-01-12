e5619c19c4480c2706e32c0e01a763750414d557
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-01-12 11:59:52 -0800

    add base js function

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
 $(document).ready(function () {
   $("form#fields").submit(function(event) {
-    $(".result").show();
 
+
+
+    if (SELECTORS ADDED AND SHOW TO PATH) {
+      console.log("does if statement working?")
+      $(".result#css").show();
+      console.log("It's the css result!")
+    } else {
+        $(".result").show();
+        console.log("Everything was false so I show everything")
+    }
+
+
+    // $(".result").show();
+
+    console.log("I made it to the end");
     event.preventDefault();
   });
 });

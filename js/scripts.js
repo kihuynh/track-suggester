a19a898bb78ad9296e4f9712af61972afd493458
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-01-12 16:36:49 -0800

    remove margin-top from .title

--- js/scripts.js
+++ js/scripts.js
@@ -7,21 +7,18 @@ $(document).ready(function () {
     var name = $("#name").val();
 
     if ((personality === "Creative") && (phone === "Android" || phone === "iPhone") &&  (frontback === "Front-end")) {
-      console.log("css path")
       $("#css").show();
     } else if ((personality === "Technical") && (phone === "Android" || phone === "iPhone")  && (frontback === "Back-end")) {
       $("#android").show();
-      console.log("android path")
     } else if ((personality === "Technical" || personality === "Creative") && (phone === "Android" || phone === "iPhone") || (frontback === "FULLSTACK")) {
-      console.log("ruby path")
       $("#ruby").show();
     } else {
         $("#everything").show();
-        console.log("else dumbtruck")
+
     }
 
     $("span.insert").text(name.toUpperCase());
-    console.log("I made it to the end");
+
     event.preventDefault();
   });
 });
